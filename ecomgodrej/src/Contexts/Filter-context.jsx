7923a514eeb01cf0962246da8cbd316f72ef81fb
aravind <aravindtirumalasetty@gmail.com>
2023-05-29 17:45:52 +0530

    Added Filter Functionality

---
+++ ecomgodrej/src/Contexts/Filter-context.jsx
@@ -0,0 +1,40 @@
+import React, {
+  useContext,
+  createContext,
+  useReducer,
+  useEffect,
+  useState,
+} from "react";
+import { filterreducer } from "../Reducers/FilterReducer";
+import { WomenAssets } from "../Components/WomenItems";
+const FilterContext = createContext();
+const FilterProvider = ({ children }) => {
+  const [searchTerm, setsearchTerm] = useState("");
+  const initialCart = {
+     originalcart:WomenAssets,
+     tempcart:[],
+  }
+  const [state, dispatch] = useReducer(filterreducer, initialCart);
+
+  console.log(state)
+  useEffect(() => {
+    dispatch({ type: "FILTER", payload:{searchTerm}});
+  },[searchTerm]);
+  return (
+    <FilterContext.Provider
+      value={{
+        ...state,
+        searchTerm,
+        setsearchTerm,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilter = () => {
+  return useContext(FilterContext);
+};
+
+export { FilterContext, FilterProvider };
